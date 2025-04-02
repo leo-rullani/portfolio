d61d0e56ff4de611977cbbc6203e78c536144821
Leugzim Rullani <leugzimrullani@outlook.com>
2025-04-02 19:05:03 +0200

    profil pic in progress

--- portfolio-app/src/app/app.component.ts
+++ portfolio-app/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { SocialMediaComponent } from './social-media/social-media.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ import { SocialMediaComponent } from './social-media/social-media.component';
     CommonModule,
     RouterOutlet,
     MenuComponent,
-    SocialMediaComponent
+    SocialMediaComponent,
+    ProfileComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
