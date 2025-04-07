d1caf2f65ff21c1aee95c5e553e9053fa774c331
Leugzim Rullani <leugzimrullani@outlook.com>
2025-04-07 11:11:41 +0200

    arrow-left good

--- portfolio-app/src/app/contact-me/contact-me.component.ts
+++ portfolio-app/src/app/contact-me/contact-me.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contact-me',
   standalone: true,
-  imports: [],
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.scss']
 })
 export class ContactMeComponent {
 
-  onArrowLeftClick() {
-    window.scrollBy({ top: 0, left: -window.innerWidth, behavior: 'smooth' });
+  @Input() scrollEl!: ElementRef<HTMLDivElement>;
+
+  scrollPrev() {
+    // Prüfen, ob wir tatsächlich den Container haben
+    if (!this.scrollEl?.nativeElement) {
+      console.warn('No nativeElement on scrollEl');
+      return;
+    }
+
+    // Dann scrollen wir den Container um eine Bildschirmbreite NACH LINKS weiter
+    this.scrollEl.nativeElement.scrollBy({
+      left: -window.innerWidth,
+      behavior: 'smooth'
+    });
   }
 }
